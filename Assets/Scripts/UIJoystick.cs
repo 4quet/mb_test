f68d49c95d047a9dbb8d70b6219d57dba04ff346
Louis Fourquet <louis.fourquet@gmail.com>
2025-08-09 12:42:54 +0200

    UI joystick showing input direction

--- Assets/Scripts/UIJoystick.cs
+++ Assets/Scripts/UIJoystick.cs
@@ -5,18 +5,31 @@ public class UIJoystick : MonoBehaviour
     [SerializeField]
     private GameInput _gameInput;
 
+    [SerializeField]
+    private RectTransform _innerStick;
+
     private RectTransform _rectTransform;
+    private float _innerStickOffset;
 
     private void Start()
     {
         _rectTransform = transform as RectTransform;
+        _innerStickOffset = _rectTransform.rect.width / 4.0f;
     }
 
     private void Update()
     {
-        if(_gameInput.IsPointerPressed && (Vector2)_rectTransform.position != _gameInput.PointerPressStartPosition)
+        if(_gameInput.IsPointerPressed)
         {
-            _rectTransform.position = _gameInput.PointerPressStartPosition;
+            if((Vector2)_rectTransform.position != _gameInput.PointerPressStartPosition)
+                _rectTransform.position = _gameInput.PointerPressStartPosition;
+
+            Vector3 direction = (_gameInput.PointerPosition - _gameInput.PointerPressStartPosition).normalized;
+            _innerStick.position = _rectTransform.position + direction * _innerStickOffset;
+        }
+        else if(_innerStick.position != _rectTransform.position)
+        {
+            _innerStick.position = _rectTransform.position;
         }
     }
 }
